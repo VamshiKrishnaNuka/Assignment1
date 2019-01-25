7aa244dfd33a617b7db3c91a68dc11901ffc4937
vamshi nuka <Vamshinuka@live.in>
2019-01-24 21:08:52 -0600

    first loop

--- Program.cs
+++ Program.cs
@@ -6,17 +6,42 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            
-            
 
-            var age=0;
-            var wiight=0.0;
-            var healthy=false;
-            var EXIT_CHAR='q';
-            decimal BMI;
-            var String="";
-            
-            Console.WriteLine("Hello there!");
+
+
+            var age = 0;
+            var weight = 0.0;
+            var adult = false;
+            var EXIT_CHAR = 'q';
+            decimal BMI = 0.0m;
+            var name = "";
+
+            Console.WriteLine("Please enter your name ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age");
+            age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Welcome {name} to BMI survey");
+            Console.WriteLine("Are you above 18? reply true or false ");
+            adult = bool.Parse(Console.ReadLine());
+
+            while (adult == true)
+            {
+
+
+
+
+
+
+
+
+            }
+
+
+            Console.ReadLine();
+
+
 
         }
     }
