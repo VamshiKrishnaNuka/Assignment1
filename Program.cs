1979e01eea66017de1bd67c910d6aca91ac869a5
vamshi nuka <Vamshinuka@live.in>
2019-01-24 21:31:57 -0600

    2nd loop

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace Assignment1
         static void Main(string[] args)
         {
 
-
-
             var age = 0;
+            var dif = 0;
+            var height = 0;
             var weight = 0.0;
             var adult = false;
             var EXIT_CHAR = 'q';
@@ -21,23 +21,20 @@ namespace Assignment1
 
             Console.WriteLine("Please enter your age");
             age = Int32.Parse(Console.ReadLine());
-
-            Console.WriteLine($"Welcome {name} to BMI survey");
-            Console.WriteLine("Are you above 18? reply true or false ");
-            adult = bool.Parse(Console.ReadLine());
-
-            while (adult == true)
+            
+            if (age >= 18)
             {
-
-
-
-
-
-
-
-
+                Console.WriteLine($"Welcome {name} to BMI Calculator");
+                Console.WriteLine("Please enter your weight in Kgs");
+                weight = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter your height");
+                height = Int32.Parse(Console.ReadLine());
             }
-
+            else
+            {
+                dif = 18 - age;
+                Console.WriteLine($"{name} please try again after {dif} years ");
+            }
 
             Console.ReadLine();
 
